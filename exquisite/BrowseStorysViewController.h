097b6da7500782c73aba5d2aafa106c19da08693
Duncan Marsh <dwolfm@gmail.com>
2015-02-23 13:52:13 -0800

    added  WriteSegmentVC, TimeLineVC, BrowseStorysVC, and ReadStoryVC to story board and generated vc class files for each

---
+++ exquisite/BrowseStorysViewController.h
@@ -0,0 +1,13 @@
+//
+//  BrowseStorysViewController.h
+//  exquisite
+//
+//  Created by drwizzard on 2/23/15.
+//  Copyright (c) 2015 nacnud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BrowseStorysViewController : UIViewController
+
+@end
--- exquisite/WriteSegmentViewController.h
+++ exquisite/WriteSegmentViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  WriteSegmentViewController.h
 //  exquisite
 //
 //  Created by drwizzard on 2/23/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface WriteSegmentViewController : UIViewController
 
 @end
-
